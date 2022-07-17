06c05cc7d23bc61975acc6c3315b8194f196f320
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-17 20:38:32 +0100

    setup menu buttons

--- src/components/main_menu.py
+++ src/components/main_menu.py
@@ -1,13 +1,21 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext
-from utils.text import text
+from utils.text import text, button
 import logging
 
 
 def display(update: Update, context: CallbackContext):
     user_id = update.effective_chat.id
+    keyboard = [
+        [button('book_session')],
+        [button('my_info'), button('settings')]
+    ]
 
-    update.effective_message.reply_text(text('main_menu'), parse_mode='HTML')
+    update.effective_message.reply_text(text('main_menu'),
+                                        reply_markup=ReplyKeyboardMarkup(
+                                            keyboard,
+                                            resize_keyboard=True),
+                                        parse_mode='HTML')
     logging.info("Main page for user_id %s with update_id %s",
                  user_id, update.update_id)
 
