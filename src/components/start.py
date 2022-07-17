daf94491312f846d81596eb2968b13a5c51ee575
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-17 22:33:14 +0100

    Refactor /start command callback function

--- src/components/start.py
+++ src/components/start.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import ChatAction, Update
 from telegram.ext import CallbackContext
 from utils.text import text
 from db.queries import create_table_if_not_exists, add_new_user, get_user
@@ -6,6 +6,7 @@ from .register import register_user
 from src.constants import STATUS
 from src.components import main_menu
 import logging
+import time
 
 
 # ----------------------------------------------------------------
@@ -23,26 +24,30 @@ def start(update: Update, context: CallbackContext):
     create_table_if_not_exists('users')
 
     # Fetch data from Database
+
+    if len(get_user(user_id)) == 0:
+        add_new_user(user_id)
+        update.effective_message.reply_text(text('welcome'),
+                                            parse_mode='HTML')
+        context.bot.send_chat_action(user_id, action=ChatAction.TYPING)
+        time.sleep(2)
+        logging.info(
+            'New user was added to the database, user_id: %s', user_id)
+
     db_user_data = get_user(user_id)
+    user_status = db_user_data[0][-1]
+
+    if user_status == STATUS[5]:
+        update.effective_message.reply_text(
+            text('welcome_back'), parse_mode='HTML')
+        logging.info("Active user restarted the bot, user_id: %s", user_id)
+        return main_menu.display(update, context)
 
     context.user_data.update(
         {
             "user_id": user_id,
-            "current_status": STATUS[1],
+            "current_status": user_status,
         }
     )
-
-    if len(db_user_data) == 0:
-        add_new_user(user_id)
-        update.effective_message.reply_text(text('welcome'),
-                                            parse_mode='HTML')
-        logging.info(
-            'New user was added to the database, user_id: %s', user_id)
-
-    # May create a bug if the data is not submitted to the DB from context.user_data
-    if get_user(user_id, 'status')[0] == STATUS[5]:
-        update.effective_message.reply_text(
-            text('welcome_back'), parse_mode='HTML')
-        return main_menu.display(update, context)
 
     return register_user(update, context)
