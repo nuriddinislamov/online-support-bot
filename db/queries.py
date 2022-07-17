a47b3e172d0ad905f2616a94093e1257b98dd2b9
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-17 22:32:36 +0100

    add new queries for fetching user data

--- db/queries.py
+++ db/queries.py
@@ -4,7 +4,7 @@ from src.constants import STATUS
 
 def create_table_if_not_exists(name):
     db_query(
-        f"CREATE TABLE IF NOT EXISTS {name} (id INTEGER PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), phone_number INTEGER(12), level VARCHAR(30), status VARCHAR(20))")
+        f"CREATE TABLE IF NOT EXISTS {name} (id INTEGER PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), phone_number VARCHAR(12), level VARCHAR(30), status VARCHAR(20))")
 
 
 def add_new_user(id: int):
@@ -12,10 +12,24 @@ def add_new_user(id: int):
         f"INSERT INTO users (id, status) VALUES ({id}, 'new_user')")
 
 
+def reset_user(id: int):
+    db_query(f"DELETE FROM users WHERE id = ({id})")
+    add_new_user(id)
+
+
 def get_user(id: int, column: str = None):
     return db_query(f"SELECT {'*' if column is None else column} FROM users WHERE id = {id}")
 
 
-def set_user(id: int, column: str, value):
+def set_user(id: int, data: dict):
+    # dict = {
+    #     'first_name': "Nuriddin",
+    #     'last_name': "Islamov",
+    #     "phone": "801-801-801-801"
+    # }
+    pairs = []
+    for i in data:
+        pairs.append(f"{i} = '{data[i]}'")
+
     db_query(
-        f"UPDATE users SET {column} = {value} WHERE id = {id}")
+        f"UPDATE users SET {', '.join(pairs)} WHERE id = {id}")
