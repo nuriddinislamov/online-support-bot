890b8071abc27a8a06042c794a5b4c403838b369
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-18 13:19:55 +0100

    condense main menu to single display() function

--- src/components/main_menu.py
+++ src/components/main_menu.py
@@ -8,39 +8,16 @@ def display(update: Update, context: CallbackContext):
     user_id = update.effective_chat.id
     keyboard = [
         [button('book_session')],
-        [button('my_info'), button('settings')]
+        [button('settings')]
     ]
 
-    update.effective_message.reply_text(text('main_menu'),
-                                        reply_markup=ReplyKeyboardMarkup(
-                                            keyboard,
-                                            resize_keyboard=True),
-                                        parse_mode='HTML')
+    update.effective_message.reply_text(
+        text('main_menu'),
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True),
+        parse_mode='HTML')
     logging.info("Main page for user_id %s with update_id %s",
                  user_id, update.update_id)
 
     return "MAIN_MENU"
-
-
-def book_session(update: Update, context: CallbackContext):
-    user_id = update.effective_chat.id
-
-    update.effective_message.reply_text("I am Nuriddin", parse_mode='HTML')
-
-    return "BOOK_SESSION"
-
-
-def my_info(update: Update, context: CallbackContext):
-    user_id = update.effective_chat.id
-
-    update.effective_message.reply_text("I am Nuriddin", parse_mode='HTML')
-
-    return "MY_INFO"
-
-
-def settings(update: Update, context: CallbackContext):
-    user_id = update.effective_chat.id
-
-    update.effective_message.reply_text("I am Nuriddin", parse_mode='HTML')
-
-    return "SETTINGS"
